524ae630236b4d5e611fb7899f8e5274f30603c3
larry <kiplarry109@gmail.com>
2024-04-24 18:03:52 +0000

    fixed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([
-    createUser(),
-    uploadPhoto(),
-  ]).then((res) => {
-    console.log(res[1].body, res[0].firstName, res[0].lastName);
-  }).catch(() => {
-    console.log('Signup system offline');
-  });
+  const pic = uploadPhoto();
+  const user = createUser();
+
+  return Promise
+    .all([pic, user]).then((resolve) => {
+      console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
