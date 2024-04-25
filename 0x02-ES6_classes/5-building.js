6ffaffa2d6d3b1052e3a7afcb5dd83279f1f4a58
kiplrry <kiplarry19@gmail.com>
2024-04-26 01:23:05 +0300

    linting

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,10 @@
 export default class Building {
   constructor(sqft) {
-      this.sqft = sqft;
-      if (this.constructor !== Building && 
-        typeof this.evacuationWarningMessage !== 'function') {
-          throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
+    this.sqft = sqft;
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
