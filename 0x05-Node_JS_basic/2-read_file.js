1582dcbf0d1ecd51e44e5e84257c41c92faba459
kiplrry <kiplarry19@gmail.com>
2024-06-17 19:02:09 +0300

    all

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -6,7 +6,12 @@ function countStudents(path) {
     const data = fs.readFileSync(path, 'utf-8');
     const lines = data.toString().split('\n');
     lines.shift();
+    let number = 0
     lines.forEach((line) => {
+      if(!line){
+        return
+      }
+      number++
       const field = line.split(',').pop();
       const student = line.split(',').shift();
       if (!fields[field]) {
@@ -15,14 +20,14 @@ function countStudents(path) {
       fields[field].push(student);
     });
 
-    console.log(`Number of students: ${lines.length}`);
+    console.log(`Number of students: ${number}`);
     for (const field in fields) {
       if (fields[field]) {
         console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
       }
     }
   } catch (err) {
-    throw Error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 
