d779e1a4e681b4c84f576786d9ea1b0270096f66
kiplrry <kiplarry19@gmail.com>
2024-06-18 15:54:27 +0300

    4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const { createServer } = require('node:http');
+
+const hostname = 'localhost';
+const port = 1245;
+
+const app = createServer((req, res) => {
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello Holberton School!');
+});
+app.listen(port, hostname);
+module.exports = app;
