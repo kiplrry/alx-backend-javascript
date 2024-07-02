92a25f20a141aa921617b3191c3666a5cccfe8dd
kiplrry <kiplarry19@gmail.com>
2024-07-02 15:47:41 +0300

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,9 +1,15 @@
+const http = require('http');
 const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+const path = process.argv[2];
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     const fields = {};
     let number = 0;
+    let ans = '';
     fs.readFile(path, (err, data) => {
       if (err) {
         reject(Error('Cannot load the database'));
@@ -24,14 +30,30 @@ function countStudents(path) {
         fields[field].push(student);
       });
 
-      console.log(`Number of students: ${number}`);
+      ans += `Number of students: ${number}\n`;
+      const list = [];
       for (const field in fields) {
         if (fields[field]) {
-          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+          list.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
         }
       }
-      resolve(data);
+      ans += list.join('\n');
+      resolve(ans);
     });
   });
 }
-module.exports = countStudents;
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+    return;
+  }
+  if (req.url === '/students') {
+    const ans = await countStudents(path);
+    res.write('This is the list of our students\n');
+    res.end(ans);
+  }
+});
+
+app.listen(port, hostname);
+module.exports = app;
